ba4cf0d5fbdbec9190d0e2115705148080cef8fa
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-16 19:03:12 -0300

    feat: create Category entity with JPA mapping

--- src/main/java/com/sebastiao/eventmanager/entities/Category.java
+++ src/main/java/com/sebastiao/eventmanager/entities/Category.java
@@ -1,4 +1,50 @@
 package com.sebastiao.eventmanager.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
